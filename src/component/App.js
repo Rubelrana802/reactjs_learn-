766b86bf24d5fee6f7842339183132386ea0ca52
rubelrana802 <mdrubelrana112@gmail.com>
2019-03-30 23:51:06 +0600

    First Class

--- src/component/App.js
+++ src/component/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import First from './Firstclass/Firstlearn'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <First />
+        <First />
+        <First />
       </div>
     );
   }
---
+++ src/component/Firstclass/Firstlearn.js
@@ -0,0 +1,17 @@
+import React, { component } from 'react'
+
+class First extends React.Component {
+
+	render() {
+		return (
+			<div>
+				<h3>I am a Rubel Rana</h3>
+				<p>I am a Student</p>
+				<p>I am a Software Enginner</p>
+			</div>
+		)
+	}
+
+}
+
+export default First;
